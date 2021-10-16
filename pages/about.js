da1911bc3cad69ee7404858c887e2bc0d52a0d2f
hamdy <hamdyyousseif32@gmail.com>
2021-10-16 21:22:18 +0200

    initial

---
+++ pages/about.js
@@ -0,0 +1,21 @@
+import Head from "next/head";
+const About = () => {
+  return (
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+      </Head>
+      <div>
+        <h1>about</h1>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis
+          iure enim molestias voluptas ratione, blanditiis commodi aspernatur
+          consectetur architecto dolorum recusandae dolorem, veniam quidem odit
+          repellendus eveniet dignissimos repellat animi.
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default About;
